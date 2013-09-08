c033ff2eca70230f676f8bd95d0bd5ecd89948a4
vitor <vitorh45@gmail.com>
2013-09-08 02:23:26 -0300

    ajustes

--- django-cielo-exemplo/wsgi.py
+++ django-cielo-exemplo/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miniature.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django-cielo-exemplo.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
